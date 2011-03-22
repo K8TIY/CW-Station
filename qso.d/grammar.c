3df334b65268b5537c027a156c541f20908bd59f
Moses Hall <moses@blugs.com>
2011-03-22 14:41:22 -0400

    Adjust formatting.

--- qso.d/grammar.c
+++ qso.d/grammar.c
@@ -4,7 +4,7 @@ static int is_vowel(char first_char);
 
 char buffer[200];
 
-char* A_Or_An (char* string)
+char* A_Or_An(char* string)
 {
   if (is_vowel (string[0]) == 1) sprintf (buffer, "an %s", string);
   else sprintf (buffer, "a %s", string);
