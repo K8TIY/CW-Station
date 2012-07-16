ebcf1f2da3760d63fe411609c4b14d8fdda77256
K8TIY <moses@blugs.com>
2012-07-16 19:00:16 -0400

    Add/update copyright and GPL info.

--- Levenshtein.h
+++ Levenshtein.h
@@ -1,3 +1,19 @@
+/*
+Copyright © 2010-2012 Brian S. Hall
+Loosely based on NString-Levenshtein by Rick Bourner
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 or later as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 #import <Cocoa/Cocoa.h>
 
 @interface Levenshtein : NSObject
--- Levenshtein.m
+++ Levenshtein.m
@@ -1,9 +1,9 @@
 /*
-Copyright © 2010-2011 Brian S. Hall
+Copyright © 2010-2012 Brian S. Hall
 Loosely based on NString-Levenshtein by Rick Bourner
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2 as
+it under the terms of the GNU General Public License version 2 or later as
 published by the Free Software Foundation.
 
 This program is distributed in the hope that it will be useful,
@@ -14,7 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
-
 #import "Levenshtein.h"
 
 static unsigned SmallestOf(unsigned a, unsigned b, unsigned c);
